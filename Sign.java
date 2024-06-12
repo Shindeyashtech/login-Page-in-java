c42b01f29f45251250a2c7ee8bcaf06057f2a45e
Yash shinde <yashshinde.tech@gmail.com>
2024-06-12 23:13:03 +0530

    login

---
+++ Sign.java
@@ -0,0 +1,20 @@
+package SignUp;
+
+import java.util.Scanner;
+
+public class Sign {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your Name : ");
+		String name = sc.next();
+		System.out.println("Enter Your Password : ");
+		String password  = sc.next();
+
+//System.out.println(name);
+sc.close();
+	}
+
+}
